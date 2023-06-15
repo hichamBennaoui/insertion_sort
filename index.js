2f6691bfc4d5ab140a76dcfa32e180d8da6ba5f0
hichamBennaoui <hicham.bennaoui@gmail.com>
2023-06-15 17:46:52 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function isrtion() {
+    
+ }
+
+for (i = 1; i < list.length; i++) {
+  j = i - 1;
+  cle = list[i];
+  while (j >= 0 && list[j] > cle) {
+    list[j] = list[j + 1];
+    j--;
+  }
+  list[j + 1] = cle;
+}
+
+arry = [23, 4, 21, 11, 34, 26, 10]
+ function isrtion(arry);
+ console.log(arry);
